e123d4beb123776f5e6bee6d761983303cc8fc2b
Dzenan <dcausevic@localhost.localdomain>
2015-04-23 13:22:43 +0200

    Fix formatting

--- ImageresizeComponent.php
+++ ImageresizeComponent.php
@@ -5,9 +5,9 @@ namespace App\Controller\Component;
 use Cake\Controller\Component;
 
 /*
- * 
+ *
  * CakePHP component for image resizing
- * 
+ *
  * Author: Dzenan Causevic
  * Date: April 23, 2015
  * Link: https://github.com/dcausevic/imageresize
@@ -21,13 +21,10 @@ use Cake\Controller\Component;
  * for more details: http://www.gnu.org/licenses/gpl.html
  *
  */
-
 class ImageresizeComponent extends Component {
-
-    var $image;
-    var $image_type;
-
-    public function load($filename) {
+	var $image;
+	var $image_type;
+	public function load($filename) {
 		$image_info = getimagesize ( $filename );
 		$this->image_type = $image_info [2];
 		if ($this->image_type == IMAGETYPE_JPEG) {
@@ -40,7 +37,6 @@ class ImageresizeComponent extends Component {
 			$this->image = imagecreatefrompng ( $filename );
 		}
 	}
-	
 	function save($filename, $image_type = IMAGETYPE_JPEG, $compression = 75, $permissions = null) {
 		if ($image_type == IMAGETYPE_JPEG) {
 			imagejpeg ( $this->image, $filename, $compression );
@@ -56,7 +52,6 @@ class ImageresizeComponent extends Component {
 			chmod ( $filename, $permissions );
 		}
 	}
-	
 	function output($image_type = IMAGETYPE_JPEG) {
 		if ($image_type == IMAGETYPE_JPEG) {
 			imagejpeg ( $this->image );
@@ -68,33 +63,27 @@ class ImageresizeComponent extends Component {
 			imagepng ( $this->image );
 		}
 	}
-	
 	function getWidth() {
 		return imagesx ( $this->image );
 	}
-	
 	function getHeight() {
 		return imagesy ( $this->image );
 	}
-	
 	function resizeToHeight($height) {
 		$ratio = $height / $this->getHeight ();
 		$width = $this->getWidth () * $ratio;
 		$this->resize ( $width, $height );
 	}
-	
 	function resizeToWidth($width) {
 		$ratio = $width / $this->getWidth ();
 		$height = $this->getheight () * $ratio;
 		$this->resize ( $width, $height );
 	}
-	
 	function scale($scale) {
 		$width = $this->getWidth () * $scale / 100;
 		$height = $this->getheight () * $scale / 100;
 		$this->resize ( $width, $height );
 	}
-	
 	function resize($width, $height) {
 		$new_image = imagecreatetruecolor ( $width, $height );
 		imagecopyresampled ( $new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth (), $this->getHeight () );
